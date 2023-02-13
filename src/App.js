ed1c8a2d811ab7b5c4c085e93772e2eeacf5e800
Victor <andre.victor60@free.fr>
2023-02-13 13:11:09 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from "./assets/img/axians.png";
 import Container from "react-bootstrap/Container";
 import Badge from "react-bootstrap/Badge";
 import { useState } from "react";
-import { Form, InputGroup, Row,Button, ListGroup } from "react-bootstrap";
+import { Form, InputGroup, Row, Button, ListGroup } from "react-bootstrap";
 
 function App() {
   const [url, setUrl] = useState("");
   const [temperature, setTemperature] = useState("");
   const [id, setId] = useState("");
+  const [data, setData] = useState([]);
 
   const handleCheck = (e) => {
     e.preventDefault();
@@ -19,20 +20,28 @@ function App() {
     if (search_params.has("d")) {
       let nameParam = search_params.get("d");
       let newStr = nameParam.replace("C", "");
-      let temperature = parseInt(newStr) / 10 - 40;
+      var temperature = parseInt(newStr) / 10 - 40;
       setTemperature(temperature);
-      console.log(temperature);
     } else {
       setTemperature("Introuvable");
     }
 
     if (search_params.has("i")) {
       let nameParam = search_params.get("i");
+      var id = nameParam;
       setId(nameParam);
-      console.log(id);
     } else {
       setId("Introuvable");
     }
+    const updateData = [
+      ...data,
+      {
+        id: id,
+        temp: temperature,
+        date: new Date().toLocaleString()
+      },
+    ];
+    setData(updateData);
   };
 
   return (
@@ -41,18 +50,17 @@ function App() {
         <Row>
           <InputGroup className="mb-3">
             <Form.Control
-                           className="form-control"
-                           type="url"
-                           name="url"
-                           id="url"
-                           onChange={(e) => setUrl(e.target.value)}
-                           value={url}
-                           placeholder="https://example.com"
+              className="form-control"
+              type="url"
+              name="url"
+              id="url"
+              onChange={(e) => setUrl(e.target.value)}
+              value={url}
+              placeholder="https://example.com"
             />
-          <Button type="submit" variant="primary" id="button-addon2">
-            Update
-          </Button>
-
+            <Button type="submit" variant="primary" id="button-addon2">
+              Update
+            </Button>
           </InputGroup>
         </Row>
       </Form>
@@ -60,11 +68,28 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </Row>
       <Row>
-      <ListGroup>
-        <ListGroup.Item><span>ID : </span> {id}</ListGroup.Item>
-        <ListGroup.Item><span>Temperature : </span><Badge bg="warning">{temperature} °C</Badge></ListGroup.Item>
-      </ListGroup>
-
+        <ListGroup>
+          <ListGroup.Item>
+            <span>ID : </span> {id}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <span>Temperature : </span>
+            <Badge bg="warning">{temperature} °C</Badge>
+          </ListGroup.Item>
+        </ListGroup>
+      </Row>
+      <Row className="mt-5">
+        <h2>{console.log("data", data)}Log temperature</h2>
+        <ListGroup>
+          {data
+          .map((i) => (
+            <ListGroup.Item key={i.id}>
+              <p>
+              <span>Date :</span> {i.date}  <span>Temperature :</span> {i.temp} °C ({id})
+              </p>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </Row>
     </Container>
   );
